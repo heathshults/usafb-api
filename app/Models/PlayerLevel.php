a6729c3ac6156c774b5cbe6e1c6ea64c7a23ba26
Sudakatux <jstuartmilne@gmail.com>
2017-07-18 19:29:49 -0300

    feat: Player level entity and model

---
+++ app/Models/PlayerLevel.php
@@ -0,0 +1,19 @@
+<?php
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class PlayerLevel extends Model
+{
+    protected $table = 'player_level';
+
+    protected $fillable = [];
+
+    protected $dates = [];
+
+    public static $rules = [
+        // Validation rules
+    ];
+
+    // Relationships
+}
