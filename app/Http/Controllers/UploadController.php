e52e46d565f7410da8f970c9bce3ae0b7571ecc0
Sudakatux <jstuartmilne@gmail.com>
2017-07-28 10:46:08 -0300

    refactor: docs NFC

--- app/Http/Controllers/UploadController.php
+++ app/Http/Controllers/UploadController.php
@@ -8,14 +8,14 @@ class UploadController extends Controller
 {
     const TYPE = 'PLAYER';
     /**
-     Will retun a json object with processed and errors keys
-     @param Illuminate\Http\Request $request with a csv_file
-     @return JsonObject
-     {
-         processes: n,
-         errros: i
-     }
-     holding the amount of processed items `n` and the amount of errors `i`
+     * Will retun a json object with processed and errors keys
+     * @param Illuminate\Http\Request $request with a csv_file
+     * @return JsonObject
+     * {
+     *    processes: n,
+     *    errros: i
+     * }
+     * holding the amount of processed items `n` and the amount of errors `i`
      */
     public function processFileUpload(Request $request)
     {
