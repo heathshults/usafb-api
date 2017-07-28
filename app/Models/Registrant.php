5c2959ce950b62e6e5d9dde1c0cb6287767b73db
Sudakatux <jstuartmilne@gmail.com>
2017-07-28 10:45:30 -0300

    feat: Added registrant model and migrations

---
+++ app/Models/Registrant.php
@@ -0,0 +1,42 @@
+<?php
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Facades\DB;
+
+class Registrant extends Model
+{
+    protected $table = 'registrant';
+    
+    protected $fillable = [];
+
+    protected $dates = [];
+
+    public static $rules = [
+        // Validation rules
+    ];
+    /**
+        Adds a create listener for model
+    */
+    protected static function boot()
+    {
+        parent::boot();
+
+        static::creating(function ($model) {
+            $usadfId = self::generateUsadfbId();
+            $model->usadfb_id  = $usadfId;
+        });
+    }
+    /**
+        Will generate an id for Usadfb.
+        This is a temporary function, the generation of this id hasnt been discussed
+        This is just some ideas grabed from daily meetings
+    */
+    public static function generateUsadfbId()
+    {
+        $maxPlayerId = DB::table('registrant')
+                        ->find(DB::table('registrant')->max('id'));
+        $newSequence = is_null($maxPlayerId) ? 0 : $maxPlayerId->id + 1;
+        return date('Y', time()).str_pad($newSequence, 16, '0', STR_PAD_LEFT);
+    }
+}
