a6bc33164b84323e9420163c602e0fb182bb97fb
Matias Blanco <matias.blanco@tecnorespuestas.com.ar>
2017-08-04 17:37:47 -0300

    Changed array() to []

--- app/Http/Services/ImportCsv/ImportCsvService.php
+++ app/Http/Services/ImportCsv/ImportCsvService.php
@@ -29,10 +29,10 @@ class ImportCsvService
         $header = fgetcsv($fd);
         
         if (!ImportCsvUtils::isLineAsExpected($header)) {
-            return array(
+            return [
                 'processed' => 0,
                 'error' => 1
-            );
+            ];
         }
         
         $indexMapperArray = ImportCsvUtils::columnToIndexMapper($header);
@@ -76,8 +76,8 @@ class ImportCsvService
             }
         }
 
-        return array('processed' => $linesProcessed,
-                     'errors' => $errors);
+        return ['processed' => $linesProcessed,
+                'errors' => $errors];
     }
 
     /**
@@ -90,80 +90,80 @@ class ImportCsvService
         $parseToDate = ImportCsvUtils::toClojure('parseToDate');
         $testNotRequired = ImportCsvUtils::toClojure('testNotRequired');
         $identity = FunctionalHelper::toClojure('App\Helpers\FunctionalHelper', 'identity');
-        $rules = array(
-                'address' => array('rule' => $testRequired,
+        $rules = [
+                'address' => ['rule' => $testRequired,
                     'field_name' => 'address_first_line',
-                    'tables' => array('App\Models\Registrant')),
-                'address_line_2' => array('rule' => $identity,
+                    'tables' => ['App\Models\Registrant']],
+                'address_line_2' => ['rule' => $identity,
                     'field_name' => 'address_second_line',
-                    'tables' => array('App\Models\Registrant')),
-                'cell_phone' => array('rule' => $testRequired,
+                    'tables' => ['App\Models\Registrant']],
+                'cell_phone' => ['rule' => $testRequired,
                     'field_name' => 'phone_number',
-                    'tables' => array( 'App\Models\Registrant')),
-                'city' => array('rule' => $testRequired,
+                    'tables' => [ 'App\Models\Registrant']],
+                'city' => ['rule' => $testRequired,
                     'field_name' => 'city',
-                    'tables' => array('App\Models\Registrant')),
-                'county' => array('rule' => $testRequired,
+                    'tables' => ['App\Models\Registrant']],
+                'county' => ['rule' => $testRequired,
                     'field_name' => 'county',
-                    'tables' => array('App\Models\Registrant')),
-                'date_of_birth' => array('rule' => FunctionalHelper::compose($testRequired, $parseToDate),
+                    'tables' => ['App\Models\Registrant']],
+                'date_of_birth' => ['rule' => FunctionalHelper::compose($testRequired, $parseToDate),
                     'field_name' => 'birth_date',
-                    'tables' => array('App\Models\Registrant')),
-                'email' => array('rule' => $testRequired,
+                    'tables' => ['App\Models\Registrant']],
+                'email' => ['rule' => $testRequired,
                     'field_name' => 'email',
-                    'tables' => array('App\Models\Registrant')),
-                'first_name' => array('rule' => $testRequired,
+                    'tables' => ['App\Models\Registrant']],
+                'first_name' => ['rule' => $testRequired,
                     'field_name' => 'first_name',
-                    'tables' => array('App\Models\Registrant')) ,
-                'game_type' =>  array('rule' => $testRequired,
+                    'tables' => ['App\Models\Registrant']] ,
+                'game_type' =>  ['rule' => $testRequired,
                     'field_name' => 'game_type',
-                    'tables' => array('App\Models\Registrant')),
-                'gender' => array('rule' => $testRequired,
+                    'tables' => ['App\Models\Registrant']],
+                'gender' => ['rule' => $testRequired,
                     'field_name' => 'gender',
-                    'tables' => array('App\Models\Registrant')),
-                'last_name' => array('rule' => $testRequired,
+                    'tables' => ['App\Models\Registrant']],
+                'last_name' => ['rule' => $testRequired,
                     'field_name' => 'last_name',
-                    'tables' => array('App\Models\Registrant')),
-                'level_of_play'=> array('rule' => $testRequired,
+                    'tables' => ['App\Models\Registrant']],
+                'level_of_play'=> ['rule' => $testRequired,
                     'field_name' => 'level_of_play',
-                    'tables' => array('App\Models\Registrant')),
-                'middle_name' => array('rule' => $identity,
+                    'tables' => ['App\Models\Registrant']],
+                'middle_name' => ['rule' => $identity,
                     'field_name' => 'middle_name',
-                    'tables' => array('App\Models\Registrant')),
-                'usadfb_id' => array('rule' => $testNotRequired ,
+                    'tables' => ['App\Models\Registrant']],
+                'usadfb_id' => ['rule' => $testNotRequired ,
                     'field_name' => 'usadfb_id',
-                    'tables' => array('App\Models\Registrant', 'App\Models\Player')),
-                'state' => array('rule' => $testRequired,
+                    'tables' => ['App\Models\Registrant', 'App\Models\Player']],
+                'state' => ['rule' => $testRequired,
                     'field_name' => 'state',
-                    'tables' => array( 'App\Models\Registrant')),
-                'zip' => array('rule' => $testRequired,
+                    'tables' => [ 'App\Models\Registrant']],
+                'zip' => ['rule' => $testRequired,
                     'field_name' => 'zip_code',
-                    'tables' => array('App\Models\Registrant')),
-                'current_grade' => array('rule' => $testRequired,
+                    'tables' => ['App\Models\Registrant']],
+                'current_grade' => ['rule' => $testRequired,
                     'field_name' => 'grade',
-                    'tables' => array('App\Models\Player')),
-                'height' => array('rule' => $testRequired,
+                    'tables' => ['App\Models\Player']],
+                'height' => ['rule' => $testRequired,
                     'field_name' => 'height',
-                    'tables' => array('App\Models\Player')),
-                'high_school_grad_year' => array('rule' => $testRequired,
+                    'tables' => ['App\Models\Player']],
+                'high_school_grad_year' => ['rule' => $testRequired,
                     'field_name' => 'graduation_year',
-                    'tables' => array('App\Models\Player')),
-                'instagram_handle' => array('rule' => $identity,
+                    'tables' => ['App\Models\Player']],
+                'instagram_handle' => ['rule' => $identity,
                     'field_name' => 'instagram',
-                    'tables' => array('App\Models\Player')),
-                'other_sports_played' => array('rule' => $testRequired,
+                    'tables' => ['App\Models\Player']],
+                'other_sports_played' => ['rule' => $testRequired,
                     'field_name' => 'sports',
-                    'tables' => array('App\Models\Player')),
-                'twitter_handle' => array('rule' => $identity,
+                    'tables' => ['App\Models\Player']],
+                'twitter_handle' => ['rule' => $identity,
                     'field_name' => 'twitter',
-                    'tables' => array('App\Models\Player')),
-                'weight' => array('rule' => $testRequired,
+                    'tables' => ['App\Models\Player']],
+                'weight' => ['rule' => $testRequired,
                     'field_name' => 'weight',
-                    'tables' => array('App\Models\Player')),
-                '#_years_in_sport' => array('rule' => $testRequired,
+                    'tables' => ['App\Models\Player']],
+                '#_years_in_sport' => ['rule' => $testRequired,
                     'field_name' => 'years_at_sport',
-                    'tables' => array('App\Models\Player'))
-            );
+                    'tables' => ['App\Models\Player']]
+            ];
             return $rules;
     }
 }
