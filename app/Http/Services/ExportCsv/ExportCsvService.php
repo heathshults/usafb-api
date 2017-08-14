bb39d315fed68a312f990c0778af5b97d52dd252
Matias Blanco <matias.blanco@tecnorespuestas.com.ar>
2017-08-14 15:54:19 -0300

    Modified some export tables/columns

--- app/Http/Services/ExportCsv/ExportCsvService.php
+++ app/Http/Services/ExportCsv/ExportCsvService.php
@@ -17,29 +17,54 @@ class ExportCsvService
                                         "array_to_string(ARRAY(SELECT 'r' || '.' || c.column_name
                                             FROM information_schema.columns As c
                                                 WHERE table_name = 'registrant' 
-                                                AND  c.column_name NOT IN('id')
+                                                AND  c.column_name NOT IN('id', 'created_at', 'updated_at')
                                         ), ',') || ',' || " .
                                         "array_to_string(ARRAY(SELECT 'c' || '.' || c.column_name FROM ".
                                         "information_schema.columns As c WHERE table_name = 'coach' AND " .
-                                        " c.column_name NOT IN('id', 'registrant_id')), ',') as columns";
-    const EXPORT_QUERY_COACH = 'SELECT %s from registrant r inner join coach c on c.registrant_id = r.id';
+                                        " c.column_name NOT IN('id', 'registrant_id', 'created_at', 'updated_at')), ',') || ',' || ".
+                                        "array_to_string(ARRAY(SELECT 'reg' || '.' || c.column_name
+                                            FROM information_schema.columns As c
+                                                WHERE table_name = 'registration' 
+                                                AND c.column_name NOT IN('id', 'source_id', 'registrant_id', 'type', 'created_at', 'updated_at')
+                                        ), ',') || ',' || " .
+                                        "array_to_string(ARRAY(SELECT 'cr' || '.' || c.column_name
+                                            FROM information_schema.columns As c
+                                                WHERE table_name = 'coach_registration' 
+                                                AND c.column_name NOT IN('id', 'source_id', 'registration_id', 'created_at', 'updated_at')
+                                        ), ',') as columns";
+    const EXPORT_QUERY_COACH = 'SELECT %s from registrant r '.
+                               'inner join coach c on c.registrant_id = r.id '.
+                               'inner join (SELECT DISTINCT on (registrant_id) * FROM '.
+                               'registration order BY registrant_id, created_at DESC) reg on '.
+                                'reg.registrant_id = r.id '.
+                               'inner join coach_registration cr on cr.registration_id = reg.id ';
 
     const EXPORT_QUERY_PLAYER_COLUMNS = "SELECT " .
                                         "array_to_string(ARRAY(SELECT 'r' || '.' || c.column_name
                                             FROM information_schema.columns As c
                                                 WHERE table_name = 'registrant' 
-                                                AND  c.column_name NOT IN('id')
+                                                AND  c.column_name NOT IN('id', 'created_at', 'updated_at')
                                         ), ',') || ',' || " .
                                         "array_to_string(ARRAY(SELECT 'p' || '.' || c.column_name FROM " .
                                         "information_schema.columns As c WHERE table_name = 'player' " .
-                                        "AND  c.column_name NOT IN('id', 'registrant_id')), ',') || ',' || " .
+                                        "AND  c.column_name NOT IN('id', 'registrant_id', 'created_at', 'updated_at')), ',') || ',' || " .
+                                        "array_to_string(ARRAY(SELECT 'reg' || '.' || c.column_name
+                                            FROM information_schema.columns As c
+                                                WHERE table_name = 'registration' 
+                                                AND c.column_name NOT IN('id', 'source_id', 'registrant_id', 'type', 'created_at', 'updated_at')
+                                        ), ',') || ',' || " .
+                                        "array_to_string(ARRAY(SELECT 'pr' || '.' || c.column_name
+                                            FROM information_schema.columns As c
+                                                WHERE table_name = 'player_registration' 
+                                                AND c.column_name NOT IN('id', 'source_id', 'registration_id', 'created_at', 'updated_at')
+                                        ), ',') || ',' || " .
                                         "array_to_string(ARRAY(SELECT 'pg' || '.' || c.column_name FROM ".
                                         "information_schema.columns As c WHERE table_name = ".
                                         "'parent_guardian' AND  c.column_name NOT IN('id', ".
-                                        "'player_registration_id')), ',') as columns";
+                                        "'player_registration_id', 'created_at', 'updated_at')), ',') as columns";
     const EXPORT_QUERY_PLAYER = 'SELECT %s from registrant r ' .
                                 'inner join player p on p.registrant_id = r.id '.
-                                'inner join (SELECT DISTINCT on (registrant_id) registrant_id, id FROM '.
+                                'inner join (SELECT DISTINCT on (registrant_id) * FROM '.
                                 'registration order BY registrant_id, created_at DESC) reg on '.
                                 'reg.registrant_id = r.id '.
                                 'inner join player_registration pr on pr.registration_id = reg.id '.
@@ -70,6 +95,31 @@ class ExportCsvService
         $result = DB::select(sprintf($queryBody, $columns[0]->columns));
         $array = json_decode(json_encode($result), true);
 
-        return $array;
+        $data = [];
+        $currentIndex = -1;
+        $lastUsaFbId = null;
+        $parentCount = 1;
+        foreach ($array as $key => $value) {
+            if($lastUsaFbId !== $value['usafb_id']) {
+                $lastUsaFbId = $value['usafb_id'];
+                $currentIndex += 1;
+                $parentCount = 1;
+            }
+
+            foreach ($value as $column => $columnValue) {
+                if(strrpos($column, 'pg_') !== false) {
+                    $newKey = $column . '_' . $parentCount;
+                } else {
+                    $newKey = $column;
+                }
+
+                $data[$currentIndex][$newKey] = $columnValue; 
+            }
+
+            $parentCount += 1;
+
+        }
+
+        return $data;
     }
 }
