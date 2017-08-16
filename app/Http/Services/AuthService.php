65ad8620703d859c9d2cfa5cc784b615b0a5028b
Matias Blanco <matias.blanco@tecnorespuestas.com.ar>
2017-08-16 11:38:31 -0300

    Fixed authentication issue

--- app/Http/Services/AuthService.php
+++ app/Http/Services/AuthService.php
@@ -24,10 +24,11 @@ use App\Models\Enums\Role;
  */
 class AuthService
 {
-    protected $token;
-    protected $tokenInfo;
     protected $management;
     protected $authentication;
+
+    const EXISTENT_USER_MSG = "The email address submitted already exists in the system.";
+    const PERMISSION_DENIED_MSG = "Permission denied.";
 
     /**
      * Initialize authentication client with auth credentials
@@ -63,18 +64,6 @@ class AuthService
             $this->management = new Management($accessToken, getenv('AUTH_DOMAIN'));
         }
         return $this->management;
-    }
-
-    /**
-     * Verify user is authenticated by token provided in header
-     *
-     * @param array $requestHeaders
-     *
-     * @return void
-     */
-    public function authenticate($requestHeaders)
-    {
-        $this->token = $this->getHeaderToken($requestHeaders);
     }
 
     /**
@@ -128,23 +117,24 @@ class AuthService
                 )
             );
         } catch (ClientException $e) {
-            throw new UnauthorizedHttpException('Authentication', 'Wrong email or password.');
+            throw new UnauthorizedHttpException('Authentication', 'Invalid email or password.');
         }
     }
 
     /**
-     * Get user profile by token provided in header
+     * Get authenticated user profile by token provided in header
      *
      * @param array $requestHeaders
      *
      * @return json
      */
-    public function getUser()
+    public function authenticatedUser($requestHeaders)
     {
+        $token = $this->getHeaderToken($requestHeaders);
         try {
-            return $this->authentication->userinfo($this->token);
+            return $this->authentication->userinfo($token);
         } catch (ClientException $e) {
-            throw new UnauthorizedHttpException('Bearer', 'Invalid token.');
+            throw new UnauthorizedHttpException('Authentication', 'Invalid token.');
         }
     }
 
@@ -152,33 +142,56 @@ class AuthService
      * Determines if a user is U.S. Soccer Staff
      * This user will have full access to all api endpoints except deletion
      *
+     * @param array $user
+     *
      * @return boolean
      */
     public function isSuperUser($user)
     {
-        $metadata = $user[getenv('AUTH_METADATA')];
-        if (isset($metadata["roles"])) {
-            foreach ($metadata["roles"] as $roleId) {
-                if ($roleId == Role::USSF_ADMIN) {
-                    return true;
-                }
-            }
-        }
-        return false;
+        return $this->hasRoles($user, [Role::label(Role::USSF_ADMIN)]);
+    }
+
+    /**
+     * Determines if a user is Pro Club Admin
+     * This user will have full access to all api endpoints
+     * but limited data
+     *
+     * @param array $user
+     *
+     * @return boolean
+     */
+    public function isProClubAdmin($user)
+    {
+        return $this->hasRoles($user, [Role::label(Role::PRO_CLUB_ADMIN)]);
     }
 
     /**
      * Determines if a user is Automation Test user
      * This user will have full access to all api endpoints
      *
+     * @param array $user
+     *
      * @return boolean
      */
     public function isTestUser($user)
     {
+        return $this->hasRoles($user, [Role::label(Role::TEST)]);
+    }
+
+    /**
+     * Determines if a user has role
+     *
+     * @param array $user
+     * @param array $roles
+     *
+     * @return boolean
+     */
+    public function hasRoles($user, $roles)
+    {
         $metadata = $user[getenv('AUTH_METADATA')];
         if (isset($metadata["roles"])) {
-            foreach ($metadata["roles"] as $roleId) {
-                if ($roleId == Role::TEST) {
+            foreach ($metadata["roles"] as $roleName) {
+                if (is_array($roles) && in_array($roleName, $roles)) {
                     return true;
                 }
             }
@@ -189,51 +202,25 @@ class AuthService
     /**
      * Create user
      *
-     * @param array $user
+     * @param array $newUser
      *
      * @return json
      */
     public function createUser($newUser)
     {
-
-        $user = $this->getUser();
-        if ($this->isSuperUser($user)) {
-            try {
-                $userCreated = $this->getManagement()->users->create($newUser);
-                $this->authentication->dbconnections_change_password(
-                    $userCreated["email"],
-                    getenv('AUTH_CONNECTION')
-                );
-                return $userCreated;
-            } catch (ClientException $e) {
-                $message = $e->getMessage();
-                if (strpos($message, 'The user already exists') !== false) {
-                    $message = "The email address submitted already exists in the system.";
-                }
-                throw new ConflictHttpException($message);
+        try {
+            $userCreated = $this->getManagement()->users->create($newUser);
+            $this->authentication->dbconnections_change_password(
+                $userCreated["email"],
+                getenv('AUTH_CONNECTION')
+            );
+            return $userCreated;
+        } catch (ClientException $e) {
+            $message = $e->getMessage();
+            if (strpos($message, 'The user already exists') !== false) {
+                $message = self::EXISTENT_USER_MSG;
             }
-        } else {
-            throw new AccessDeniedHttpException("Permission denied.");
-        }
-    }
-
-    /**
-     * Change Password by user token
-     *
-     * @param integer $id
-     *
-     * @return json
-     */
-    public function changePassword($id, $password)
-    {
-        $user = $this->getUser();
-        if ($this->isSuperUser($user)) {
-            $data = [
-                "password" => $password
-            ];
-            return $this->updateUser($id, $data);
-        } else {
-            throw new AccessDeniedHttpException("Permission denied.");
+            throw new ConflictHttpException($message);
         }
     }
 
@@ -246,30 +233,7 @@ class AuthService
      */
     public function getUserById($id)
     {
-        $user = $this->getUser();
-        if ($this->isSuperUser($user)) {
-            return $this->getManagement()->users->get($id);
-        } else {
-            throw new AccessDeniedHttpException("Permission denied.");
-        }
-    }
-
-    /**
-     * Get all Users
-     *
-     * @param string $token
-     *
-     * @return json
-     */
-    public function getAllUsers()
-    {
-        $user = $this->getUser();
-        if ($this->isSuperUser($user)) {
-            $users = $this->getManagement()->users->getAll([]);
-            return $users;
-        } else {
-            throw new AccessDeniedHttpException("Permission denied.");
-        }
+        return $this->getManagement()->users->get($id);
     }
 
     /**
@@ -281,73 +245,15 @@ class AuthService
      */
     public function deleteUser($id)
     {
-        $user = $this->getUser();
-        if ($this->isTestUser($user)) {
-            if (empty($id)) {
-                throw new BadRequestHttpException("Invalid id");
-            }
-            $userToDelete = $this->getUserById($id);
-            return $this->getManagement()->users->delete($id);
-        } else {
-            throw new AccessDeniedHttpException("Permission denied");
+        if (empty($id)) {
+            throw new BadRequestHttpException("Invalid id");
         }
+        $userToDelete = $this->getUserById($id);
+        return $this->getManagement()->users->delete($id);
     }
 
     /**
-     * Get roles
-     *
-     * @return json
-     */
-    public function getRoles()
-    {
-
-        $user = $this->getUser();
-        if ($this->isSuperUser($user)) {
-            return response()->json(Role::labels());
-        } else {
-            throw new AccessDeniedHttpException("Permission denied");
-        }
-    }
-
-    /**
-     * Send email to reset password
-     *
-     * @param string $email
-     *
-     * @return json
-     */
-    public function forgotPassword($email)
-    {
-        $user = $this->getUserByEmail($email);
-        if ($user !== null) {
-            $emailSentMessage = $this->authentication->dbconnections_change_password(
-                $email,
-                getenv('AUTH_CONNECTION')
-            );
-            return response()->json(["message" => $emailSentMessage]);
-        }
-        throw new NotFoundHttpException("User not found");
-    }
-
-    /**
-     * Get user by criteria
-     *
-     * @param string $email
-     *
-     * @return json
-     */
-    public function getUserByEmail($email)
-    {
-        $options = [
-            'search_engine' => 'v2',
-            'q' => "email.raw:".$email
-        ];
-        $users = $this->getManagement()->users->getAll($options);
-        return empty($users) ? null : $users[0];
-    }
-
-    /**
-     * Set authentication client
+     * Delete user by id
      *
      * @param Auth0\SDK\API\Authentication $authentication
      *
@@ -368,5 +274,193 @@ class AuthService
     public function setManagement($management)
     {
         $this->management =  $management;
+    }
+
+    /**
+     * Get fields that belongs to user metadata
+     *
+     * @return array
+     */
+    protected function metadataFields()
+    {
+        return [
+            'first_name',
+            'last_name',
+            'city',
+            'phone_number',
+            'state',
+            'postal_code',
+            'modified_by'
+        ];
+    }
+
+    /**
+     * Update user by id
+     *
+     * @param string $id User id
+     * @param array $data User data to be updated
+     *
+     * @return json
+     */
+    public function updateUser($id, $data)
+    {
+        if (isset($data['role'])) {
+            throw new BadRequestHttpException("Role can not be changed.");
+        }
+        if (isset($data['password']) || isset($data['email'])) {
+            $data['connection'] = getenv('AUTH_CONNECTION');
+        }
+        if (isset($data['email'])) {
+            $data['client_id'] = getenv('AUTH_CLIENT_ID');
+        }
+        $metadata = [];
+        $metadata_fields = $this->metadataFields();
+        foreach ($metadata_fields as $field) {
+            if (isset($data[$field])) {
+                $metadata[$field] = $data[$field];
+                unset($data[$field]);
+            }
+        }
+        if (!empty($metadata)) {
+            $data['user_metadata'] = $metadata;
+        }
+
+        try {
+            return $this->getManagement()->users->update($id, $data);
+        } catch (ClientException $e) {
+            $message = $e->getMessage();
+            if (strpos($message, 'The specified new email already exists') !== false) {
+                $message = self::EXISTENT_USER_MSG;
+                throw new ConflictHttpException($message);
+            }
+            throw $e;
+        }
+    }
+
+    /**
+     * Get all Users
+     *
+     * @param string $criteria
+     *
+     * @return json
+     */
+    public function getAllUsers($criteria)
+    {
+        $options = [
+            "search_engine" => "v2",
+            "include_totals" => true,
+            "per_page" => 10,
+            "page" => 0
+        ];
+        if (isset($criteria['page'])) {
+            $page = $criteria['page'];
+            if (isset($page['number'])) {
+                $options['page'] = $page['number'];
+            }
+            if (isset($page['size'])) {
+                $options['per_page'] = $page['size'];
+            }
+        }
+        if (isset($criteria['sort'])) {
+            $options['sort'] = $this->extractSortField($criteria['sort']);
+        }
+
+        if (isset($criteria['q'])) {
+            $options['q'] = $this->createFilterQuery($criteria['q']);
+        }
+
+        $result = $this->getManagement()->users->getAll($options);
+
+        $users = [];
+        if (!empty($result['users'])) {
+            $totalUsers = $result['total'];
+            $pageSize = intval($options['per_page']);
+            $totalPages = intval(ceil($totalUsers/$pageSize));
+            $currentPage = intval($options['page']);
+            $users = [
+                "meta" => [
+                    "total_pages" => $totalPages,
+                    "total_users" => $totalUsers
+                ],
+                "data" => $result['users'],
+                "links" => $this->createLinksPagination($currentPage, $pageSize, $totalPages)
+            ];
+        }
+        return $users;
+    }
+
+    /**
+     * Convert an string to a valid sort expression for auth0 search query
+     *
+     * @param string $sortExpression
+     *
+     * @return string sort query
+     */
+    public function extractSortField($sortExpression)
+    {
+        $field = trim($sortExpression);
+        $order = 1;
+        $firstCharacter = $field[0];
+        if ($firstCharacter == '-' || $firstCharacter == '+') {
+            $field = substr($field, 1);
+            $order = intval($firstCharacter.'1');
+        }
+        $metadata_fields = $this->metadataFields();
+        if (in_array($field, $metadata_fields)) {
+            $field = 'user_metadata.'.$field;
+        }
+        $sort = $field.':'.$order;
+        return $sort;
+    }
+
+    /**
+     * Create a valid filter expression for auth0 search query
+     *
+     * @param string $criteria
+     *
+     * @return string filter query
+     */
+    public function createFilterQuery($criteria)
+    {
+        $escapedCriteria = urlencode($criteria);
+        $filterQuery = "user_metadata.first_name:*".$escapedCriteria."*";
+        $filterQuery .= " OR user_metadata.last_name:*".$escapedCriteria."*";
+        $filterQuery .= ' OR email:"*'.$criteria.'*"';
+        $filterQuery .=" OR user_metadata.roles:*".$escapedCriteria."*";
+
+        return $filterQuery;
+    }
+
+    /**
+     * Links array for pagination that will be included in search response
+     *
+     * @param string $currentPage
+     * @param string $pageSize
+     * @param string $totalPages
+     *
+     * @return array links
+     */
+    public function createLinksPagination($currentPage, $pageSize, $totalPages)
+    {
+        $usersUrl = getenv('HOSTNAME').'/users';
+        $pageNumberParam = "?page[number]=";
+        $pageSizeParam = "&page[size]=";
+
+        $prev = null;
+        if ($currentPage !== 0) {
+            $prev = $usersUrl.$pageNumberParam.($currentPage - 1).$pageSizeParam.$pageSize;
+        }
+        $next = null;
+        $lastPage = $totalPages - 1;
+        if ($currentPage !== $lastPage) {
+            $next = $usersUrl.$pageNumberParam.($currentPage + 1).$pageSizeParam.$pageSize;
+        }
+        return [
+            "self" => $usersUrl.$pageNumberParam.$currentPage.$pageSizeParam.$pageSize,
+            "first" => $usersUrl.$pageNumberParam."0".$pageSizeParam.$pageSize,
+            "prev" => $prev,
+            "next" =>  $next,
+            "last" => $usersUrl.$pageNumberParam.$lastPage.$pageSizeParam.$pageSize
+        ];
     }
 }
--- tests/unit/UserTest.php
+++ tests/unit/UserTest.php
@@ -6,88 +6,42 @@ use Mockery;
 use App\Http\Services\AuthService;
 use \Symfony\Component\HttpKernel\Exception\UnauthorizedHttpException;
 use App\Models\Enums\Role;
+use WithoutMiddleware;
+use Tests\Helpers\MockHelper;
 
 class UserTest extends \TestCase
 {
 
     /**
-     * Test successfull get token from header
-     *
-     * @return void
-     */
-    public function testSuccefullTokenHeader()
-    {
-        $service = new AuthService();
-        $headers = [
-            'Authorization' => ['Bearer token123']
-        ];
-        $this->assertEquals('token123', $service->getHeaderToken($headers));
-    }
-
-    /**
-     * Test failed get token from header when missing token
-     *
-     * @return void
-     */
-    public function testMissingTokenHeader()
-    {
-        $service = new AuthService();
-        $headers = [];
-        $this->expectException(UnauthorizedHttpException::class);
-        $service->getHeaderToken($headers);
-    }
-
-    /**
-     * Test sucessfull get user info
+     * Test sucessfull get user info when token is provided
      *
      * @return void
      */
     public function testSuccefullGetUser()
     {
-        $mockAuth = Mockery::mock(\Auth0\SDK\API\Authentication::class);
-        $mockAuth->shouldReceive('userinfo')
-            ->andReturn(
-                [
-                    "sub" => "auth0|123",
-                    "name" => "test@gmail.com",
-                    "nickname" => "test",
-                    "picture" => "https =>//s.gravatar.com/avatar/123.png",
-                    "updated_at" => "2017-07-06T21 =>10 =>27.449Z",
-                    "email" => "test@gmail.com",
-                    "email_verified" => true,
-                    "http://ussfb.com/metadata" => array(
-                        "firstName" => "test",
-                        "lastName" => "test",
-                        "city" => "Frisco",
-                        "state" => "TX",
-                        "postalCode" => "70000",
-                        'roles' => [Role::USAFB_ADMIN]
-                    )
-                ]
-            );
+        $mockAuth = MockHelper::authenticationMock();
         $service = new AuthService();
         $service->setAuthentication($mockAuth);
         $headers = [
-            'Authorization' => 'Bearer token123'
+            'Authorization' => ['Bearer token123']
         ];
-        $data = $service->getUser($headers);
-        $this->assertEquals('auth0|123', $data['sub']);
-        $this->assertEquals('test@gmail.com', $data['name']);
-        $this->assertEquals('test', $data['nickname']);
-        $this->assertEquals('https =>//s.gravatar.com/avatar/123.png', $data['picture']);
-        $this->assertEquals('2017-07-06T21 =>10 =>27.449Z', $data['updated_at']);
-        $this->assertEquals('test@gmail.com', $data['email']);
-        $this->assertEquals(true, $data['email_verified']);
-        $this->assertEquals(
-            array(
-                "firstName" => "test",
-                "lastName" => "test",
-                "city" => "Frisco",
-                "state" => "TX",
-                "postalCode" => "70000",
-                "roles" => [Role::USAFB_ADMIN]
-            ),
-            $data['http://ussfb.com/metadata']
-        );
+        $data = $service->authenticatedUser($headers);
+        $this->assertEquals($data, MockHelper::userResponse());
+    }
+
+    /**
+     * Test exception thrown in get user info when token is not provided
+     *
+     * @return void
+     */
+    public function testFailureGetUser()
+    {
+        $this->app->instance('Auth', MockHelper::authServiceMock(false));
+
+        $response = $this->json('GET', '/me');
+
+        $response->seeJson([
+                'error' => 'Invalid token.'
+            ])->seeStatusCode(401);
     }
 }
