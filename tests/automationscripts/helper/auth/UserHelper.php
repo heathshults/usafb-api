5ee4e5d4d8ee96905eb8208330bb24db89c21b22
Jaya.Yedalla <jaya.yedalla@bluestarsports.com>
2017-08-23 14:57:00 -0500

    feature/USAFB-38 -Automation Test cases for Create User

--- tests/automationscripts/helper/auth/UserHelper.php
+++ tests/automationscripts/helper/auth/UserHelper.php
@@ -32,4 +32,55 @@ class UserHelper
         $I->sendGET($url);
         return $I->grabResponse();
     }
+
+    /**
+     * Function for post call
+     * @param ApiTester $I
+     * @param $url
+     * @param $postBody
+     * @param $token
+     * @return string
+     */
+    public function postCall(ApiTester $I, $url, $postBody, $token)
+    {
+        $I->clearHeaders();
+        $I->setHeaders();
+        $I->amBearerAuthenticated($token);
+        $I->sendPOST($url, $postBody);
+        return $I->grabResponse();
+    }
+
+    /**
+     * Function get call for userID
+     * @param ApiTester $I
+     * @param $url
+     * @param $token
+     * @param $userID
+     * @return string
+     */
+    public function getUserByID(ApiTester $I, $url, $token, $userID)
+    {
+        $I->clearHeaders();
+        $I->setHeaders();
+        $I->amBearerAuthenticated($token);
+        $I->sendGET(str_replace("|", "%7C", $url));
+        return $I->grabResponse();
+    }
+
+    /**
+     * Function for delete call
+     * @param ApiTester $I
+     * @param $url
+     * @param $token
+     * @param $userID
+     * @return string
+     */
+    public function deleteUserByID(ApiTester $I, $url, $token, $userID)
+    {
+        $I->clearHeaders();
+        $I->setHeaders();
+        $I->amBearerAuthenticated($token);
+        $I->sendDELETE(str_replace("|", "%7C", $url));
+        return $I->grabResponse();
+    }
 }
--- tests/automationscripts/validators/auth/UserValidator.php
+++ tests/automationscripts/validators/auth/UserValidator.php
@@ -21,4 +21,46 @@ class UserValidator
         $expectedResponseArr = $common->convertJsonToArray($expectedResponse);
         $common->assertObjects($actualResponseArr, $expectedResponseArr, $I);
     }
+
+    /**
+     * Function to validate actual response with excepted response
+     * @param $actualResponse
+     * @param $expectedResponse
+     * @param ApiTester $I
+     * @param CommonUtils $common
+     */
+    public function validateResponse($actualResponse, $expectedResponse, ApiTester $I, CommonUtils $common)
+    {
+        $actualResponseArr = $common->convertJsonToArray($actualResponse);
+        $expectedResponseArr = $common->convertJsonToArray($expectedResponse);
+        $common->assertObjects($actualResponseArr, $expectedResponseArr, $I);
+    }
+
+    /**
+     * Function to assert expected key value not equal to actual key value
+     * @param $actualResponse
+     * @param $key
+     * @param $expectedVal
+     * @param ApiTester $I
+     * @param CommonUtils $common
+     */
+    public function validateKeyNotEquals($actualResponse,$key,$expectedVal, ApiTester $I, CommonUtils $common)
+    {
+        $actualResponseArr = $common->convertJsonToArray($actualResponse);
+        $common->assertNotEqualsKey($actualResponseArr,$key,$expectedVal,$I);
+    }
+
+    /**
+     * Function to assert expected key value  equal to actual key value
+     * @param $actualResponse
+     * @param $key
+     * @param $expectedVal
+     * @param ApiTester $I
+     * @param CommonUtils $common
+     */
+    public function validateKeyEquals($actualResponse,$key,$expectedVal, ApiTester $I, CommonUtils $common)
+    {
+        $actualResponseArr = $common->convertJsonToArray($actualResponse);
+        $common->assertEqualsKey($actualResponseArr,$key,$expectedVal,$I);
+    }
 }
