6ad632c47a1142a42c152df563e895d980ebb908
Noel Geren <noel.geren@bluestarsports.com>
2018-01-03 08:26:38 -0600

    Fixed issue where 1-* values were not properly being validated.

--- app/Models/Coach.php
+++ app/Models/Coach.php
@@ -70,7 +70,7 @@ class Coach extends BaseModel
         'years_experience' => 'required|numeric|min:0|max:50',
         'level' => 'required|in:youth,middle_school,freshman,jv,varsity,college,professional',
         'level_type' => 'required|in:youth_flag,7on7,rookie_tackle,11_player_tackle,adult_flag,other',
-        'positions' => 'sometimes|in:head_coach,quaterback_coach,wide_receiver_coach,linebacker_coach,
+        'positions.*' => 'sometimes|in:head_coach,quaterback_coach,wide_receiver_coach,linebacker_coach,
         offensive_coordinator,special_teams,assistant_coach,tight_end_coach,
         running_back_coach, defensive_back_coach,defensive_cooridnator',
         'organization_name' => 'required',
