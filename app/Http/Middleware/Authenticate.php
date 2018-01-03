96b5d6ce4d92bd15b13717c2fd6ddae6bce93ecf
Noel Geren <noel.geren@bluestarsports.com>
2018-01-03 08:24:51 -0600

    Updated documentation. Changed/modified authentication to work with Cognito implementation.

--- app/Http/Middleware/Authenticate.php
+++ app/Http/Middleware/Authenticate.php
@@ -4,30 +4,44 @@ namespace App\Http\Middleware;
 
 use Closure;
 use Symfony\Component\HttpKernel\Exception\UnauthorizedHttpException;
+use Illuminate\Contracts\Auth\Factory as Auth;
+use Log;
 
 class Authenticate
 {
+    /**
+     * The authentication guard factory instance.
+     *
+     * @var \Illuminate\Contracts\Auth\Factory
+     */
+    protected $auth;
+
+    /**
+     * Create a new middleware instance.
+     *
+     * @param  \Illuminate\Contracts\Auth\Factory  $auth
+     * @return void
+     */
+    public function __construct(Auth $auth)
+    {
+        $this->auth = $auth;
+    }
+
     /**
      * Handle an incoming request.
      *
      * @param  \Illuminate\Http\Request  $request
      * @param  \Closure  $next
+     * @param Guard|null
      *
      * @throws UnauthorizedHttpException if user could not be authenticated.
      * @return mixed
      */
-    public function handle($request, Closure $next)
+    public function handle($request, Closure $next, $guard = null)
     {
-        $headers = $request->headers->all();
-        $user = app('Auth')->authenticatedUser($headers);
-        if (!isset($user)) {
-            throw new UnauthorizedHttpException('Bearer', 'Invalid token.');
+        if ($this->auth->guard($guard)->guest()) {
+            throw new UnauthorizedHttpException('', 'User Not Authenticated');
         }
-        $request->setUserResolver(
-            function () use ($user) {
-                return $user;
-            }
-        );
         return $next($request);
     }
 }
