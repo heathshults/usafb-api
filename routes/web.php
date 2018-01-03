d64f9b2db74a706d1e1fa79ac9f92bd6c1d7d1b0
Noel Geren <noel.geren@bluestarsports.com>
2018-01-03 08:31:32 -0600

    Added authentication and registrat;5Cion update endpoints. Enabled authentication middleware.

--- routes/web.php
+++ routes/web.php
@@ -12,147 +12,66 @@ use App\Models\Enums\Role;
 |
 */
 
-// general
-$app->get('healthz', 'StatusController@health');
-$app->get('statusz', 'StatusController@status');
+// authentication/session
+$app->post('/auth/login', 'AuthenticationController@login');
+$app->post('/auth/activate-user', 'AuthenticationController@activateUser');
+$app->post('/auth/refresh-token', 'AuthenticationController@refreshToken');
+$app->post('/auth/forgot-password', 'AuthenticationController@forgotPassword');
+$app->post('/auth/forgot-password-confirm', 'AuthenticationController@confirmForgotPassword');
 
-// users
-// forgot-password, login
-$app->get('/users', 'UsersController@index'); 
-$app->get('/users/{id}', 'UsersController@show');
-$app->put('/users/{id}', 'UsersController@update');
-$app->put('/users/{id}/activate', 'UsersController@activate');
-$app->put('/users/{id}/deactivate', 'UsersController@deactivate');
-$app->post('/users', 'UsersController@create');
-$app->delete('/users/{id}', 'UsersController@destroy');
+// public/status
+$app->get('/healthz', 'StatusController@health');
+$app->get('/statusz', 'StatusController@status');
 
-// roles
-$app->get('/roles', 'RolesController@index');
-$app->post('/roles', 'RolesController@create');
-$app->get('/roles/permissions', 'RolesController@permissions');
-$app->get('/roles/{id}', 'RolesController@show');
-$app->put('/roles/{id}', 'RolesController@update');
-$app->delete('/roles/{id}', 'RolesController@destroy');
-
-// players
-$app->get('/players/search', 'PlayersController@search');
-$app->get('/players/{id}', 'PlayersController@show');
-$app->put('/players/{id}', 'PlayersController@update');
-$app->delete('/players/{id}', 'PlayersController@destroy');
-$app->post('/players', 'PlayersController@create');
-
-// player registrations
-$app->get('/players/{player_id}/registrations', 'PlayerRegistrationsController@index');
-$app->post('/players/{player_id}/registrations', 'PlayerRegistrationsController@create');
-$app->get('/players/{player_id}/registrations/{id}', 'PlayerRegistrationsController@show');
-    
-// coaches
-$app->get('/coaches/search', 'CoachesController@search');
-$app->get('/coaches/{id}', 'CoachesController@show');
-$app->put('/coaches/{id}', 'CoachesController@update');
-$app->delete('/coaches/{id}', 'CoachesController@destroy');
-$app->post('/coaches', 'CoachesController@create');
-
-// coach registrations
-$app->get('/coaches/{coach_id}/registrations/{id}', 'CoachRegistrationsController@show');
-$app->get('/coaches/{coach_id}/registrations', 'CoachRegistrationsController@index');
-$app->post('/coaches/{coach_id}/registrations', 'CoachRegistrationsController@create');
-
-/*
 $app->group(
-    ['middleware' => 'authenticate'],
+    ['middleware' => 'auth'],
     function () use ($app) {
-        $app->group(
-            ['middleware' =>
-                [
-                    'authorize:'.Role::SUPER_USER
-                ]
-            ],
-            function () use ($app) {
-                $app->get(
-                    '/users/{id}',
-                    [
-                        'uses' => 'UsersController@getById'
-                    ]
-                );
-                $app->get(
-                    '/users/{id}/logs',
-                    [
-                        'uses' => 'UsersController@getLogs'
-                    ]
-                );
-                $app->get(
-                    '/users',
-                    [
-                        'uses' => 'UsersController@getAll'
-                    ]
-                );
-                $app->post(
-                    '/users',
-                    [
-                        'uses' => 'UsersController@create'
-                    ]
-                );
-                $app->put(
-                    '/users/{id}',
-                    [
-                        'uses' => 'UsersController@update'
-                    ]
-                );
-                $app->post(
-                    '/reset-password',
-                    [
-                        'uses' => 'AuthenticationController@resetPassword'
-                    ]
-                );
-            }
-        );
-        $app->group(
-            ['middleware' =>
-                [
-                    'authorize:'.Role::SUPER_USER.','.Role::ADMIN_USER
-                ]
-            ],
-            function () use ($app) {
-                $app->get(
-                    '/registrants/export',
-                    [
-                        'uses' => 'DownloadController@downloadFile'
-                    ]
-                );
-                $app->post(
-                    '/registrants/import',
-                    [
-                        'uses' => 'UploadController@processFileUpload'
-                    ]
-                );
-            }
-        );
-        $app->get(
-            '/me',
-            [
-                'uses' => 'AuthenticationController@getAuthenticatedUser'
-            ]
-        );
-        $app->group(
-            ['middleware' =>
-                [
-                    'authorize:'.Role::SUPER_USER.','.Role::TEST
-                ]
-            ],
-            function () use ($app) {
-                $app->delete(
-                    '/users/{id}',
-                    [
-                        'uses' => 'UsersController@delete'
-                    ]
-                );
-            }
-        );
+        // users
+        $app->get('/users', 'UsersController@index'); 
+        $app->get('/users/{id}', 'UsersController@show');
+        $app->put('/users/{id}', 'UsersController@update');
+        $app->put('/users/{id}/activate', 'UsersController@activate');
+        $app->put('/users/{id}/deactivate', 'UsersController@deactivate');
+        $app->post('/users', 'UsersController@create');
+        $app->delete('/users/{id}', 'UsersController@destroy');
+
+        // roles
+        $app->get('/roles', 'RolesController@index');
+        $app->post('/roles', 'RolesController@create');
+        $app->get('/roles/permissions', 'RolesController@permissions');
+        $app->get('/roles/{id}', 'RolesController@show');
+        $app->put('/roles/{id}', 'RolesController@update');
+        $app->delete('/roles/{id}', 'RolesController@destroy');
+
+        // coaches
+        $app->get('/coaches/search', 'CoachesController@search');
+        $app->get('/coaches/{id}', 'CoachesController@show');
+        $app->put('/coaches/{id}', 'CoachesController@update');
+        $app->delete('/coaches/{id}', 'CoachesController@destroy');
+        $app->post('/coaches', 'CoachesController@create');
+        
+        // coach registrations
+        $app->get('/coaches/{coach_id}/registrations', 'CoachRegistrationsController@index');
+        $app->post('/coaches/{coach_id}/registrations', 'CoachRegistrationsController@create');
+        $app->get('/coaches/{coach_id}/registrations/{id}', 'CoachRegistrationsController@show');
+        $app->put('/coaches/{coach_id}/registrations/{id}', 'CoachRegistrationsController@update');
+        
+        // players
+        $app->get('/players/search', 'PlayersController@search');
+        $app->get('/players/{id}', 'PlayersController@show');
+        $app->put('/players/{id}', 'PlayersController@update');
+        $app->delete('/players/{id}', 'PlayersController@destroy');
+        $app->post('/players', 'PlayersController@create');
+
+        // player registrations
+        $app->post('/players/{player_id}/registrations', 'PlayerRegistrationsController@create');
+        $app->get('/players/{player_id}/registrations', 'PlayerRegistrationsController@index');
+        $app->get('/players/{player_id}/registrations/{id}', 'PlayerRegistrationsController@show');
+        $app->put('/players/{player_id}/registrations/{id}', 'PlayerRegistrationsController@update');        
     }
 );
-*/
 
+// root route
 $app->get(
     '/',
     function () use ($app) {
