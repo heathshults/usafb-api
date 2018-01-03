83d82f692aedd4da1828b93497b45695e39a5685
Noel Geren <noel.geren@bluestarsports.com>
2018-01-03 08:22:59 -0600

    Added update endpoint for updating player registrations. Updated phpdoc/method documentation to reflect endpoint/method parameters.

--- app/Http/Controllers/PlayerRegistrationsController.php
+++ app/Http/Controllers/PlayerRegistrationsController.php
@@ -19,17 +19,22 @@ class PlayerRegistrationsController extends Controller
     /**
      * Returns the player registration records
      *
+     * @param Request $request
+     * @param string $playerId
+     *
      * @return string (json) containing the Player Registration resources limited to 50 results per-page/request
      */
     public function index(Request $request, $playerId)
     {
+        // find Player record (or error)
         $player = Player::find($playerId);
-        // validate player record
         if (is_null($player)) {
             return $this->respond('NOT_FOUND', ['error' => ['message' => 'Player ('.$playerId.') not found.']]);
         }
         
         $sort = $this->buildSortCriteria($request->query(), [ 'column' => 'created_at', 'order' => 'desc' ]);
+        
+        // find Player Registration records ordered by *sort*
         $registrations = $player->registrations()->sortBy($sort['column']);
         
         return $this->respond('OK', $registrations);
@@ -38,17 +43,21 @@ class PlayerRegistrationsController extends Controller
     /**
      * Return the Player Registration record for the specified Player ID and Registration ID
      *
+     * @param Request $request
+     * @param string $id
+     * @param string $playerId
+     *
      * @return string (json) containing the Player Registration resource OR corresponding error message
      */
     public function show(Request $request, $id, $playerId)
     {
-        // validate player record
+        // find Player record (or error)
         $player = Player::find($playerId);
         if (is_null($player)) {
             return $this->respond('NOT_FOUND', ['error' => ['message' => 'Player ('.$playerId.') not found.']]);
         }
 
-        // lookup registration directly from player registration collection
+        // find Player Registration record with ID (or error)
         $registration = $player->registrations()->find($id);
         if (is_null($registration)) {
             return $this->respond('NOT_FOUND', [
@@ -64,16 +73,20 @@ class PlayerRegistrationsController extends Controller
     /**
      * Create a new player registration record
      *
+     * @param Request $request
+     * @param string $id
+     *
      * @return string (json) containing new record resource OR corresponding error message
      */
     public function create(Request $request, $id)
     {
+        // find Player record (or error)
         $player = Player::find($id);
         if (is_null($player)) {
             return $this->respond('NOT_FOUND', ['error' => ['message' => 'Player ('.$playerId.') not found.']]);
         }
         
-        // instantiate new player registraiton and validate fields
+        // instantiate new Player Registraiton and validate
         $playerRegistration = new PlayerRegistration($request->all());
         if (!$playerRegistration->valid()) {
             $errors = $playerRegistration->errors();
@@ -85,7 +98,11 @@ class PlayerRegistrationsController extends Controller
             ]);
         }
 
-        // TODO set all registrations to current = false
+        // set all past registrations current to false - change later to use indexed find/update
+        foreach ($player->registrations()->where('current', true)->all() as $registration) {
+            $registration->current = false;
+            $registration->save();
+        }
         
         // associate new embedded registration with player record
         $player->registrations()->associate($playerRegistration);
@@ -98,6 +115,56 @@ class PlayerRegistrationsController extends Controller
             return $this->respond('INVALID', [
                 'error' => [
                     'message' => 'Error creating new player registration record.',
+                    'errors' => $errors
+                ]
+            ]);
+        }
+    }
+    
+    /**
+     * Update player registration record
+     *
+     * @param Request $request
+     * @param string $id
+     * @param string $playerId
+     *
+     * @return string (json) containing new record resource OR corresponding error message
+     */
+    public function update(Request $request, $id, $playerId)
+    {
+        // find Player record (or error)
+        $player = Player::find($playerId);
+        if (is_null($player)) {
+            return $this->respond('NOT_FOUND', [
+                'error' => [
+                    'message' => 'Player ('.$playerId.') not found.'
+                ]
+            ]);
+        }
+        
+        // find Registration record (or error)
+        $registration = $player->registrations()->find($id);
+        if (is_null($registration)) {
+            return $this->respond('NOT_FOUND', [
+                'error' => [
+                    'message' => 'Registration ('.$id.') not found for Player ('.$playerId.').'
+                ]
+            ]);
+        }
+        
+        $requestJson = $request->json()->all();
+        
+        // merge/fill registration object with json values
+        $registration->fill($requestJson);
+        
+        // validate merged record and save (or error)
+        if ($registration->valid() && $registration->save()) {
+            return $this->respond('OK', $registration);
+        } else {
+            $errors = $registration->errors();
+            return $this->respond('INVALID', [
+                'error' => [
+                    'message' => 'Error updating Registration record.',
                     'errors' => $errors
                 ]
             ]);
