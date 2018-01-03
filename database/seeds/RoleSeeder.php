5582bf781ef0e8f242c8b6cd893276c4035b7287
Noel Geren <noel.geren@bluestarsports.com>
2018-01-03 08:34:48 -0600

    Added default roles.

--- database/seeds/RoleSeeder.php
+++ database/seeds/RoleSeeder.php
@@ -13,12 +13,51 @@ class RoleSeeder extends Seeder
      */
     public function run()
     {
-        // remove all player records
+        // remove all roles
+        
         Role::truncate();
         
-        $role = new Role();
-        $role->name = "Test";
-        $role->permissions = [ "create_user", "view_user" ];
-        $role->save();
+        // add default roles
+                
+        $suRole = new Role();
+        $suRole->name = 'Superuser';
+        $suRole->permissions = [
+            'export_players',
+            'import_players',
+            'import_coaches',
+            'export_coaches',
+            'manage_users',
+            'view_dashboard',
+            'view_players',
+            'view_coaches'
+        ];
+        $suRole->save();
+
+        $adminRole = new Role();
+        $adminRole->name = 'Administrator';
+        $adminRole->permissions = [
+            'export_players',
+            'import_players',
+            'import_coaches',
+            'export_coaches',
+            'manage_users',
+            'view_dashboard',
+            'view_players',
+            'view_coaches'
+        ];
+        $adminRole->save();
+
+        $stakeholderRole = new Role();
+        $stakeholderRole->name = 'Stakeholder';
+        $stakeholderRole->permissions = [
+            'export_players',
+            'import_players',
+            'import_coaches',
+            'export_coaches',
+            'view_dashboard',
+            'view_players',
+            'view_coaches'
+        ];        
+        $stakeholderRole->save();
     }
 }
