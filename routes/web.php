b91b04f4354c663ea5b49203ae964cf5236fb62b
Noel Geren <noel.geren@bluestarsports.com>
2018-01-08 16:54:59 -0600

    Added authorize middleware.

--- routes/web.php
+++ routes/web.php
@@ -24,49 +24,72 @@ $app->get('/healthz', 'StatusController@health');
 $app->get('/statusz', 'StatusController@status');
 
 $app->group(
-    ['middleware' => 'auth'],
+    ['middleware' => 'authenticate'],
     function () use ($app) {
-        // users
-        $app->get('/users', 'UsersController@index'); 
-        $app->get('/users/{id}', 'UsersController@show');
-        $app->put('/users/{id}', 'UsersController@update');
-        $app->put('/users/{id}/activate', 'UsersController@activate');
-        $app->put('/users/{id}/deactivate', 'UsersController@deactivate');
-        $app->post('/users', 'UsersController@create');
-        $app->delete('/users/{id}', 'UsersController@destroy');
+        
+        // manage users permission
+        $app->group(
+            ['middleware' => 'authorize:manage_users'],
+            function() use ($app){
+                // users
+                $app->get('/users', 'UsersController@index');        
+                $app->get('/users/{id}', 'UsersController@show');
+                $app->put('/users/{id}', 'UsersController@update');
+                $app->put('/users/{id}/activate', 'UsersController@activate');
+                $app->put('/users/{id}/deactivate', 'UsersController@deactivate');
+                $app->post('/users', 'UsersController@create');
+                $app->delete('/users/{id}', 'UsersController@destroy');
 
-        // roles
-        $app->get('/roles', 'RolesController@index');
-        $app->post('/roles', 'RolesController@create');
-        $app->get('/roles/permissions', 'RolesController@permissions');
-        $app->get('/roles/{id}', 'RolesController@show');
-        $app->put('/roles/{id}', 'RolesController@update');
-        $app->delete('/roles/{id}', 'RolesController@destroy');
+                // roles
+                $app->get('/roles', 'RolesController@index');
+                $app->post('/roles', 'RolesController@create');
+                $app->get('/roles/permissions', 'RolesController@permissions');
+                $app->get('/roles/{id}', 'RolesController@show');
+                $app->put('/roles/{id}', 'RolesController@update');
+                $app->delete('/roles/{id}', 'RolesController@destroy');
+            }
+        );
 
-        // coaches
-        $app->get('/coaches/search', 'CoachesController@search');
-        $app->get('/coaches/{id}', 'CoachesController@show');
+        // view coaches and coach registrations permission
+        $app->group(
+            ['middleware' => 'authorize:view_coaches'],
+            function() use ($app){
+                // coaches
+                $app->get('/coaches/search', 'CoachesController@search');
+                $app->get('/coaches/{id}', 'CoachesController@show');
+                
+                // coach registrations
+                $app->get('/coaches/{coach_id}/registrations', 'CoachRegistrationsController@index');
+                $app->get('/coaches/{coach_id}/registrations/{id}', 'CoachRegistrationsController@show');
+            }
+        );
+
+        // view players and player registrations permission
+        $app->group(
+            ['middleware' => 'authorize:view_players'],
+            function() use ($app){
+                // players
+                $app->get('/players/search', [ 'middleware' => 'authorize', 'uses' => 'PlayersController@search' ]);
+                $app->get('/players/{id}', 'PlayersController@show');
+                
+                // player registrations
+                $app->get('/players/{player_id}/registrations', 'PlayerRegistrationsController@index');
+                $app->get('/players/{player_id}/registrations/{id}', 'PlayerRegistrationsController@show');                
+            }
+        );
+            
+        // coaches / coach registrations modification endpoints (will be protected by third-party tokens)
+        $app->post('/coaches', 'CoachesController@create');
         $app->put('/coaches/{id}', 'CoachesController@update');
         $app->delete('/coaches/{id}', 'CoachesController@destroy');
-        $app->post('/coaches', 'CoachesController@create');
-        
-        // coach registrations
-        $app->get('/coaches/{coach_id}/registrations', 'CoachRegistrationsController@index');
         $app->post('/coaches/{coach_id}/registrations', 'CoachRegistrationsController@create');
-        $app->get('/coaches/{coach_id}/registrations/{id}', 'CoachRegistrationsController@show');
-        $app->put('/coaches/{coach_id}/registrations/{id}', 'CoachRegistrationsController@update');
+        $app->put('/coaches/{coach_id}/registrations/{id}', 'CoachRegistrationsController@update');                   
         
-        // players
-        $app->get('/players/search', 'PlayersController@search');
-        $app->get('/players/{id}', 'PlayersController@show');
+        // players / player registration modification endpoints (will be protected by third-party tokens)
         $app->put('/players/{id}', 'PlayersController@update');
         $app->delete('/players/{id}', 'PlayersController@destroy');
         $app->post('/players', 'PlayersController@create');
-
-        // player registrations
         $app->post('/players/{player_id}/registrations', 'PlayerRegistrationsController@create');
-        $app->get('/players/{player_id}/registrations', 'PlayerRegistrationsController@index');
-        $app->get('/players/{player_id}/registrations/{id}', 'PlayerRegistrationsController@show');
         $app->put('/players/{player_id}/registrations/{id}', 'PlayerRegistrationsController@update');        
     }
 );
