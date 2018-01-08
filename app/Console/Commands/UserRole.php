0f5b0fe15ab76766221d9a00b2779f6570844174
Noel Geren <noel.geren@bluestarsports.com>
2018-01-08 16:53:37 -0600

    Initial commit.

---
+++ app/Console/Commands/UserRole.php
@@ -0,0 +1,72 @@
+<?php
+
+namespace App\Console\Commands;
+
+use App\Models\User;
+use App\Models\Role;
+use App\Models\Address;
+use App\Http\Controllers\UsersController;
+
+use Illuminate\Console\Command;
+use Illuminate\Contracts\Http\Kernel;
+use Illuminate\Support\Facades\Log;
+
+class UserRole extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'user:role {email} {role_id}';
+    protected $client;
+    
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Change user with (email) to role with (role_id)';
+
+    /**
+     * Execute the console command.
+     *
+     * @return mixed
+     */
+    public function handle()
+    {
+        $email = $this->argument('email');
+        $roleId = $this->argument('role_id');
+        
+        if (is_null($email) || is_null($roleId)) {
+            $this->error('Invalid or missing user (email) or role (role_id).');
+        }
+        
+        $user = User::where([ "email" => $email ])->first();
+        if (is_null($user)) {
+            $this->error('Error finding user with email ('.$email.')');
+            return false;
+        }
+        
+        $role = Role::find($roleId);
+        if (is_null($role)) {
+            $this->error('Error finding role with ID ('.$roleId.')');
+            return false;            
+        }
+                
+        try {
+            $user->role_id = $role->id;
+            if ($user->save()) {
+                $this->info('User role successfully updated.');
+                return true;
+            } else {
+                $this->error('Unable to update user role.');
+                return false;
+            }
+        } catch (Exception $ex) {
+            $this->error('Error setting user ('.$email.') role to ('.$roleId.').');
+            $this->error($ex->getMessage());
+            return false;
+        }
+    }
+}
