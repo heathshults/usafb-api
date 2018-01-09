830dfda1d5a67ce44c55567c34167f2291900095
Noel Geren <noel.geren@bluestarsports.com>
2018-01-09 14:36:40 -0600

    Initial commit - Third party API key service.

---
+++ app/Http/Services/ApiKeyService.php
@@ -0,0 +1,86 @@
+<?php
+
+namespace App\Http\Services;
+
+use Illuminate\Http\Request;
+
+use Log;
+
+use Webpatser\Uuid\Uuid;
+
+class ApiKeyService
+{
+    const DEFAULT_ORG = "usafb";
+    
+    /**
+     * Generates Header for API call.
+     *
+     * @param $jsonBody
+     * @param $config
+     * @return $header
+     */
+    public function generateHeader($jsonBody, $config)
+    {
+        $timestamp = gmdate("Y-m-d\TH:i:s\Z");
+        $org = empty($config['org']) ? DEFAULT_ORG : $config['org'];
+
+        if (empty($config['clientId'])) {
+            trigger_error("Missing ClientID", E_USER_ERROR);
+        }
+        $clientId = $config['clientId'];
+
+        if (empty($config['key'])) {
+            trigger_error("Missing Key", E_USER_ERROR);
+        }
+        $key = $config['key'];
+
+        $payload = $timestamp.$jsonBody;
+        $sig = base64_encode(hash_hmac('sha256', $payload, $key, true));
+        $timestampHdr = "x-{$org}-timestamp";
+        
+        return [
+            'Authorization' => "Authorization: {$org} {$clientId}:{$sig}",
+            $timestampHdr => $timestamp,
+        ];
+    }
+        
+    /**
+     * Return new unique (API) Key
+     *
+     * @return $apiKey
+     */    
+    public function generateKey() {
+        return (string)Uuid::generate();
+    }
+
+    /**
+     * Return Provider ID from authentication request header.
+     *
+     * @param $authHeader
+     * @param $config
+     * @return $companyId
+     */
+    public function getProviderId($authHeader, $config)
+    {
+        $org = empty($config['org']) ? DEFAULT_ORG : $config['org'];
+        $re = "/(?<={$org} )(.*?)(?=:)/";
+        preg_match_all($re, $authHeader, $matches, PREG_SET_ORDER, 0);
+        return $matches[0][0];
+    }
+
+    public function validateRequest($authHeader, $timestamp, $jsonBody, $key)
+    {
+        $org = empty($config['org']) ? DEFAULT_ORG : $config['org'];
+        $re = "/^(.*) (.*):(.*)$/";
+        preg_match_all($re, $authHeader, $matches, PREG_SET_ORDER, 0);
+        if (empty($config['key'])) {
+            trigger_error("Missing Key", E_USER_ERROR);
+        }
+        $key = $config['key'];
+
+        $payload = $timestamp.$jsonBody;
+        $sig = base64_encode(hash_hmac('sha256', $payload, $key, true));
+
+        return $sig == $matches[0][3];
+    }
+}
---
+++ app/Providers/ApiKeyServiceProvider.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Providers;
+
+use App\Http\Services\ApiKeyService;
+
+use Illuminate\Support\ServiceProvider;
+
+class ApiKeyServiceProvider extends ServiceProvider
+{
+    /**
+     * Bootstrap the application services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        // intentially left blank.
+    }
+
+    /**
+     * Register the application services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        $this->app->singleton(ApiKeyService::class, function () {
+            return new ApiKeyService();
+        });
+
+        $this->app->alias(ApiKeyService::class, 'apiKey');
+    }
+}
