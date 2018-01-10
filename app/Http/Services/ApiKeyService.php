96660e7802e8082cc79e5ed360995fc0a2233af6
Noel Geren <noel.geren@bluestarsports.com>
2018-01-10 16:31:27 -0600

    Refactored, restructured, and reimplemented.

--- app/Http/Services/ApiKeyService.php
+++ app/Http/Services/ApiKeyService.php
@@ -2,15 +2,45 @@
 
 namespace App\Http\Services;
 
+use App\Models\Provider;
 use Illuminate\Http\Request;
-
 use Log;
-
+use Symfony\Component\HttpKernel\Exception\UnauthorizedHttpException;
 use Webpatser\Uuid\Uuid;
 
 class ApiKeyService
 {
-    const DEFAULT_ORG = "usafb";
+    public function authenticate($request)
+    {
+        Log::debug('ApiKeyService - authenticate(.) called.');
+        $providerId = $this->getProviderId($request);
+        if (is_null($providerId)) {
+            throw new UnauthorizedHttpException("Unable to determine Provider ID");
+        }
+        
+        $provider = Provider::find($providerId);
+        if (is_null($provider)) {
+            throw new UnauthorizedHttpException('Unable to find Provider with ID ('.$providerId.')');
+        }
+
+        $apiKey = $provider->api_key;
+        if (is_null($provider)) {
+            throw new UnauthorizedHttpException('Invalid or missing Provider API Key.');
+        }
+            
+        if ($this->validateRequest($apiKey, $request)) {
+            if (is_null($providerId)) {
+                throw new UnauthorizedHttpException("Unable to determine Provider ID");
+            }
+            Log::debug('ApiKeyService > authenticate( .. ) / Finding provider with ID: '.$providerId);
+            $provider = Provider::find($providerId);
+            if (is_null($provider)) {
+                throw new UnauthorizedHttpException('Unable to find Provider with ID ('.$providerId.')');
+            }
+            Log::debug('ApiKeyService > authenticate( .. ) / Success!');
+            return $provider;
+        }
+    }
     
     /**
      * Generates Header for API call.
@@ -19,28 +49,22 @@ class ApiKeyService
      * @param $config
      * @return $header
      */
-    public function generateHeader($jsonBody, $config)
+    public function generateAuthenticationHeaders($providerId, $body)
     {
         $timestamp = gmdate("Y-m-d\TH:i:s\Z");
-        $org = empty($config['org']) ? DEFAULT_ORG : $config['org'];
-
-        if (empty($config['clientId'])) {
-            trigger_error("Missing ClientID", E_USER_ERROR);
+        
+        $provider = Provider::find($providerId);
+        if (is_null($provider)) {
+            throw new Exception('Unable to find Provider with ID ('.$providerId.')');
         }
-        $clientId = $config['clientId'];
 
-        if (empty($config['key'])) {
-            trigger_error("Missing Key", E_USER_ERROR);
-        }
-        $key = $config['key'];
-
-        $payload = $timestamp.$jsonBody;
-        $sig = base64_encode(hash_hmac('sha256', $payload, $key, true));
-        $timestampHdr = "x-{$org}-timestamp";
+        $payload = $timestamp.$body;
+        $sig = base64_encode(hash_hmac('sha256', $payload, $provider->api_key, true));
+        $timestampHdr = "x-usafb-timestamp";
         
         return [
-            'Authorization' => "Authorization: {$org} {$clientId}:{$sig}",
-            $timestampHdr => $timestamp,
+            'Authorization' => "Authorization: usafb {$providerId}:{$sig}",
+            'x-usafb-timestamp' => $timestamp,
         ];
     }
         
@@ -61,27 +85,41 @@ class ApiKeyService
      * @param $config
      * @return $companyId
      */
-    public function getProviderId($authHeader, $config)
+    public function getProviderId($request)
     {
-        $org = empty($config['org']) ? DEFAULT_ORG : $config['org'];
-        $re = "/(?<={$org} )(.*?)(?=:)/";
+        $authHeader = $request->header('Authorization');
+        if (is_null($authHeader)) {
+            return null;
+        }
+        $re = "/(?<=usafb )(.*?)(?=:)/";
         preg_match_all($re, $authHeader, $matches, PREG_SET_ORDER, 0);
+        if (count($matches) == 0) {
+            return null;
+        }
         return $matches[0][0];
     }
 
-    public function validateRequest($authHeader, $timestamp, $jsonBody, $key)
+    public function validateRequest($apiKey, $request)
     {
-        $org = empty($config['org']) ? DEFAULT_ORG : $config['org'];
-        $re = "/^(.*) (.*):(.*)$/";
-        preg_match_all($re, $authHeader, $matches, PREG_SET_ORDER, 0);
-        if (empty($config['key'])) {
-            trigger_error("Missing Key", E_USER_ERROR);
+        $authHeader = $request->header('Authorization');
+        if (is_null($authHeader)) {
+            return false;
         }
-        $key = $config['key'];
-
-        $payload = $timestamp.$jsonBody;
-        $sig = base64_encode(hash_hmac('sha256', $payload, $key, true));
-
+        
+        $timestampHeader = $request->header('x-usafb-timestamp');
+        if (is_null($timestampHeader)) {
+            return false;
+        }
+        
+        preg_match_all("/^(.*) (.*):(.*)$/", $authHeader, $matches, PREG_SET_ORDER, 0);
+                
+        if (count($matches) == 0) {
+            return false;
+        }
+        
+        $body = $request->getContent();
+        $payload = $timestampHeader.$body;
+        $sig = base64_encode(hash_hmac('sha256', $payload, $apiKey, true));
         return $sig == $matches[0][3];
     }
 }
