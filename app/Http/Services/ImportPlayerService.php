f5902c7083a8b90823ce87d868c3428111d0c0ef
Noel Geren <noel.geren@bluestarsports.com>
2018-01-17 00:34:08 -0600

    Initial commit.

---
+++ app/Http/Services/ImportPlayerService.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Http\Services;
+
+use App\Models\UserLog;
+use App\Models\Enums\LogEvent;
+use Pitpit\Component\Diff\DiffEngine;
+use App\Helpers\AuthHelper;
+
+/**
+ * ImportPlayerService
+ * Import player records
+ *
+ * @package    Http
+ * @subpackage Services
+ */
+
+class ImportPlayerService implements ImportServiceInterface
+{
+}
