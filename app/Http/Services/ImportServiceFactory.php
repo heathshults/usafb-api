47d8d1c7ced1a67e3c20ac88e296b2b066472aa7
Noel Geren <noel.geren@bluestarsports.com>
2018-01-22 11:21:53 -0600

    Initial commit.

--- app/Http/Services/ImportServiceFactory.php
+++ app/Http/Services/ImportServiceFactory.php
@@ -3,8 +3,9 @@
 namespace App\Http\Services;
 
 use App\Models\Import;
-use App\Helpers\Services\ImportPlayerService;
-use App\Helpers\Services\ImportCoachService;
+use App\Http\Services\ImportPlayerService as ImportPlayerService;
+use App\Http\Services\ImportCoachService as ImportCoachService;
+use Illuminate\Support\Facades\Log;
 
 /**
  * ImportServiceFactory
@@ -18,10 +19,12 @@ class ImportServiceFactory
 {
     public static function build($recordType)
     {
+        Log::debug('Building ImportService for type ('.$recordType.')');
         if ($recordType == Import::TYPE_COACHES) {
-            return new ImportPlayerService();
-        } elseif ($recordType == Import::TYPE_PLAYERS) {
             return new ImportCoachService();
+        } elseif ($recordType == Import::TYPE_PLAYERS) {
+            Log::debug('Here / Building ImportPlayerService.');
+            return new ImportPlayerService();
         } else {
             throw new Exception('Invalid type. Unable to build factory.');
         }
