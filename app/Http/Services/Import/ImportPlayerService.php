da5e53e454e0eb771c2b490223a0f716560b468b
Noel Geren <noel.geren@bluestarsports.com>
2018-01-23 15:05:53 -0600

    Updated fields/removed whitespace..

--- app/Http/Services/Import/ImportPlayerService.php
+++ app/Http/Services/Import/ImportPlayerService.php
@@ -21,113 +21,113 @@ class ImportPlayerService extends ImportService
     const SOURCE_NUM_COLUMNS = 32;
         
     const COLUMNS = [
-        'External ID' => [ 
-            'field' => 'id_external' 
+        'External ID' => [
+            'field' => 'id_external'
         ],
-        'First Name' => [ 
-            'field' => 'name_first' 
+        'First Name' => [
+            'field' => 'name_first'
         ],
-        'Middle Name' => [ 
-            'field' => 'name_middle' 
+        'Middle Name' => [
+            'field' => 'name_middle'
         ],
-        'Last Name' => [ 
-            'field' => 'name_last' 
+        'Last Name' => [
+            'field' => 'name_last'
         ],
-        'Street 1' => [ 
-            'field' => 'address.street_1' 
+        'Street 1' => [
+            'field' => 'address.street_1'
         ],
-        'Street 2' => [ 
-            'field' => 'address.street_2' 
+        'Street 2' => [
+            'field' => 'address.street_2'
         ],
-        'City' => [ 
-            'field' => 'address.city' 
+        'City' => [
+            'field' => 'address.city'
         ],
-        'County' => [ 
-            'field' => 'address.county' 
+        'County' => [
+            'field' => 'address.county'
         ],
-        'State' => [ 
-            'field' => 'address.state' 
+        'State' => [
+            'field' => 'address.state'
         ],
-        'Postal Code' => [ 
-            'field' => 'address.postal_code' 
+        'Postal Code' => [
+            'field' => 'address.postal_code'
         ],
-        'DOB' => [ 
-            'field' => 'dob' 
+        'DOB' => [
+            'field' => 'dob'
         ],
-        'Gender' => [ 
-            'field' => 'gender' 
+        'Gender' => [
+            'field' => 'gender'
         ],
-        'Height FT' => [ 
-            'type' => 'number', 
-            'field' => 'height_ft' 
+        'Height FT' => [
+            'type' => 'number',
+            'field' => 'height_ft'
         ],
-        'Height IN' => [ 
-            'type' => 'number', 
-            'field' => 'height_in' 
+        'Height IN' => [
+            'type' => 'number',
+            'field' => 'height_in'
         ],
-        'Grade' => [ 
-            'type' => 'number', 
-            'field' => 'grade' 
+        'Grade' => [
+            'type' => 'number',
+            'field' => 'grade'
         ],
-        'Graduation Year' => [ 
-            'type' => 'number', 
-            'field' => 'graduation_year' 
+        'Graduation Year' => [
+            'type' => 'number',
+            'field' => 'graduation_year'
         ],
-        'Email' => [ 
-            'field' => 'email' 
+        'Email' => [
+            'field' => 'email'
         ],
-        'Home Phone' => [ 
-            'field' => 'phone_home' 
+        'Home Phone' => [
+            'field' => 'phone_home'
         ],
-        'Mobile Phone' => [ 
-            'field' => 'phone_mobile' 
+        'Mobile Phone' => [
+            'field' => 'phone_mobile'
         ],
-        'Work Phone' => [ 
-            'field' => 'phone_work' 
+        'Work Phone' => [
+            'field' => 'phone_work'
         ],
-        'Twitter' => [ 
-            'field' => 'social_twitter' 
+        'Twitter' => [
+            'field' => 'social_twitter'
         ],
-        'Instagram' => [ 
-            'field' => 'social_instagram' 
+        'Instagram' => [
+            'field' => 'social_instagram'
         ],
-        'Sports' => [ 
-            'type' => 'array', 
-            'field' => 'sports' 
+        'Sports' => [
+            'type' => 'array',
+            'field' => 'sports'
         ],
-        'Years Experience' => [ 
-            'type' => 'number', 
-            'field' => 'years_experience' 
-        ],   
-        'Guardian - First Name' => [ 
-            'field' => 'guardians.1.name_first' 
+        'Years Experience' => [
+            'type' => 'number',
+            'field' => 'years_experience'
         ],
-        'Guardian - Last Name' => [ 
-            'field' => 'guardians.1.name_last' 
+        'Guardian - First Name' => [
+            'field' => 'guardians.1.name_first'
         ],
-        'Guardian - Address Street 1' => [ 
-            'field' => 'guardians.1.address.street_1' 
+        'Guardian - Last Name' => [
+            'field' => 'guardians.1.name_last'
         ],
-        'Guardian - Address Street 2' => [ 
-            'field' => 'guardians.1.address.street_2' 
+        'Guardian - Address Street 1' => [
+            'field' => 'guardians.1.address.street_1'
         ],
-        'Guardian - Address City' => [ 
-            'field' => 'guardians.1.address.city' 
+        'Guardian - Address Street 2' => [
+            'field' => 'guardians.1.address.street_2'
         ],
-        'Guardian - Address County' => [ 
-            'field' => 'guardians.1.address.county' 
+        'Guardian - Address City' => [
+            'field' => 'guardians.1.address.city'
         ],
-        'Guardian - Address State' => [ 
-            'field' => 'guardians.1.address.state' 
+        'Guardian - Address County' => [
+            'field' => 'guardians.1.address.county'
         ],
-        'Guardian - Address Postal Code' => [ 
-            'field' => 'guardians.1.address.postal_code' 
+        'Guardian - Address State' => [
+            'field' => 'guardians.1.address.state'
+        ],
+        'Guardian - Address Postal Code' => [
+            'field' => 'guardians.1.address.postal_code'
         ],
     ];
 
-    // return new record/object (what we're importing)    
-    public function newRecord() 
+    // return new record/object (what we're importing)
+    public function newRecord()
     {
         return (new Player());
-    } 
+    }
 }
