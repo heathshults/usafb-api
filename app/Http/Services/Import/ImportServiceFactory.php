1a232612846bd30923e291f1e822e77961d98e99
Noel Geren <noel.geren@bluestarsports.com>
2018-01-23 15:04:44 -0600

    Added additional debugging/logging.

--- app/Http/Services/Import/ImportServiceFactory.php
+++ app/Http/Services/Import/ImportServiceFactory.php
@@ -19,11 +19,12 @@ class ImportServiceFactory
 {
     public static function build($recordType)
     {
-        Log::debug('Building ImportService for type ('.$recordType.')');
+        Log::debug('Getting ImportService for type ('.$recordType.')');
         if ($recordType == Import::TYPE_COACHES) {
+            Log::debug('Instantiating new coach import service.');
             return new ImportCoachService();
         } elseif ($recordType == Import::TYPE_PLAYERS) {
-            Log::debug('Here / Building ImportPlayerService.');
+            Log::debug('Instantiating new player import service.');
             return new ImportPlayerService();
         } else {
             throw new Exception('Invalid type. Unable to build factory.');
