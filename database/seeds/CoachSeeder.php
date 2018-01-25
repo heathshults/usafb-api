da95b13ae93fe45cb328e321ba8d744903faf407
Noel Geren <noel.geren@bluestarsports.com>
2018-01-24 23:28:19 -0600

    Randomize and with 'more' accurate fake data.

--- database/seeds/CoachSeeder.php
+++ database/seeds/CoachSeeder.php
@@ -26,60 +26,102 @@ class CoachSeeder extends Seeder
         Coach::truncate();
         CoachRegistration::truncate();
                 
-        for($i = 1; $i <= 500; $i++) {
-            $uid = uniqid();       
+        $firstNames = [ 'Jim', 'Jason', 'Billy', 'Brandon', 'Chris', 'JJ', 'Marky', 'Mike', 'JD', 'Don' ];
+        $firstNamesF = [ 'Sara', 'Samantha', 'Veronika', 'Shelly', 'Christa', 'Jenny', 'Jamie' ];
+        $middleNames = [ 'Lee', 'JJ', 'Sam', 'Smith', null, null ];
+        $lastNames = [ 'Smith', 'Johnson', 'Simons', 'Geren', 'Tompkins', 'Travis', 'Kawahara', 'Mitrakos', 'Boll' ];
+        $levels = [ 'youth', 'middle_school', 'freshman', 'jv', 'varsity', 'college', 'professional' ];
+        $levelTypes = [ 'youth_flag', '7on7', 'rookie_tackle', '11_player_tackle', 'adult_flag', 'other', 'flex' ];
+        $cities = [ 'Frisco', 'Plano', 'Dallas', 'Allen', 'McKinney', 'Richardson', 'Garland', 'Rockwall' ];
+        $streets1 = [ 'Main St', 'Sunshine Dr', 'Sunset Dr', 'Rolling Dr', '5th St' ];
+        $streets2 = [ 'Apt #1234', 'Suit #521', null, null ];
+        $zips = [ '75034', '75070', '75071', '75035', '75206', '75201' ];
+        $counties = [ 'Denton', 'Collin', 'Dallas' ];        
+        $positions = [ 'head_coach', 'quaterback_coach', 'wide_receiver_coach', 'linebacker_coach', 'offensive_coordinator', 'special_teams', 'assistant_coach', 'tight_end_coach', 'running_back_coach', 'defensive_back_coach', 'defensive_cooridnator', 'not_available' ];
+        $genders = [ 'M', 'F', 'NA' ];
+        $seasons = [ 'fall', 'spring', 'summer', 'winter' ];            
+
+        for($i = 1; $i <= 500; $i++) {            
+            $uid = uniqid();
+            
+            $nameMiddle = $this->randValue($middleNames);
+            $nameLast = $this->randValue($lastNames);                        
+            $gender = $this->randValue($genders);
+            $nameFirst = null;
+            if ($gender == 'F') {
+                $nameFirst = $this->randValue($firstNamesF);                
+            } else {
+                $nameFirst = $this->randValue($firstNames);
+            }
+
+            $level = $this->randValue($levels);
+            $levelType = $this->randValue($levelTypes);
+            $street1 = $this->randValue($streets1);
+            $street2 = $this->randValue($streets2);            
+            $city = $this->randValue($cities);
+            $zip = $this->randValue($zips);
+            $county = $this->randValue($counties);
+            
             $coach = new Coach();
             $coach->id_external = 'external_id_'.$i;            
             $coach->id_salesforce = 'salesforce_'.$i;            
-            $coach->name_first = 'John '.$i;
-            $coach->name_last = 'Doe';
+            $coach->name_first = $nameFirst;
+            $coach->name_middle = $nameMiddle;
+            $coach->name_last = $nameLast;
             $coach->dob = date("Y-m-d", strtotime("-1 month", strtotime("2010-01-01")));
-            $coach->email = 'john.doe'.$uid.'@gmail.com';
+            $coach->gender = $gender;
+            $coach->email = $nameFirst.'.'.$nameLast.$i.'@gmail.com';
             $coach->phone_home = '123-123-1234';
             $coach->phone_mobile = '123-123-1234';
             $coach->phone_work = '123-123-1234';
-            $coach->social_twitter = '@johndoe.'.$uid;
-            $coach->social_instagram = 'johndoeing'.$uid;
+            $coach->social_twitter = '@.'.$nameFirst.$i;
+            $coach->social_instagram = $nameFirst.$nameLast.$i;
             $coach->opt_in_marketing = true;
-            $coach->gender = 'M';
-            $coach->email = 'john.doe'.$uid.'@gmail.com';
             $coach->years_experience = rand(0,20);
             $coach->organization_name = 'Organization Name '.$i;
             $coach->organization_state = 'TX';
 
             $address = new Address();
-            $address->street_1 = '1234 Main St';
-            $address->street_2 = 'Apt #12345';
-            $address->city = 'Frisco';
+            $address->street_1 = rand(1000,9999).' '.$this->randValue($streets1);
+            $address->street_2 = $street2;
+            $address->city = $city;
             $address->state = 'TX';
-            $address->county = 'Denton';
-            $address->postal_code = '75034';
+            $address->county = $county;
+            $address->postal_code = $zip;
             $address->country = 'US';
             $coach->address()->associate($address);
 
+            $schoolName = $this->randValue($cities);
+            $position = $this->randValue($positions);
+            $season = $this->randValue($seasons);
+            
             $coach_registration = new CoachRegistration();            
             $coach_registration->coach_id = $coach->id;
             $coach_registration->id_external = 'external_id_reg_'.$i;
             $coach_registration->id_usafb = 'usafb_id_'.$i;
             $coach_registration->id_salesforce = $coach->id_salesforce;
             $coach_registration->current = true;
-            $coach_registration->position = 'head_coach';
-            $coach_registration->level = 'youth';
-            $coach_registration->level_type = 'youth_flag';
+            $coach_registration->position = $position;
+            $coach_registration->level = $level;
+            $coach_registration->level_type = $levelType;
             $coach_registration->certifications = [ 'CPR' ];
             $coach_registration->organization_name = 'Organization Name '.$i;
             $coach_registration->organization_state = 'TX';
-            $coach_registration->league_name = 'Frisco Little League';
-            $coach_registration->season_year = 2017;
-            $coach_registration->season = 'Spring';
-            $coach_registration->school_name = 'Frisco High School';
-            $coach_registration->school_district = 'Frisco ISD';
+            $coach_registration->league_name = $schoolName.' Little League';
+            $coach_registration->season_year = rand(2015,2018);
+            $coach_registration->season = $season;
+            $coach_registration->school_name = $schoolName.' High School';
+            $coach_registration->school_district = $schoolName.' ISD';
             $coach_registration->school_state = 'TX';
             $coach_registration->team_name = 'Texas Rangers';
-            $coach_registration->team_gender = 'M';
+            $coach_registration->team_gender = $gender;
             
             $coach->registrations()->associate($coach_registration);
             $coach->save();
         }
     }
+    
+    function randValue(&$values) {
+        return $values[rand(0,count($values)-1)];
+    }    
 }
